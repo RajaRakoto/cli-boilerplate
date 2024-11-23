8110e57a5b3b0d0e04f8654fa0d9358126bdad55
raja <raja.rakoto7@gmail.com>
2024-11-23 15:26:43 +0300

    chore(db): add drizzle orm config file

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+//DONT FORGET TO LOAD env variables
+import { defineConfig } from "drizzle-kit";
+
+//Configured for SQLite with Bun sqlite
+export default defineConfig({
+  dialect: "sqlite",
+  schema: "./drizzle/schema.ts",
+  out: "./drizzle/migrations",
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+  },
+  strict: true,
+  verbose: true,
+});
