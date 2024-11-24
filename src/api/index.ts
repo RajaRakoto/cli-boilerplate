8a5b59d41dc86e05ae9ef14088d536b18ce217f5
raja <raja.rakoto7@gmail.com>
2024-11-24 17:37:34 +0300

    feat(api): add google ai file manager

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 /* libs */
+import { GoogleAIFileManager } from "@google/generative-ai/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 /* orm */
@@ -6,7 +7,8 @@ import { rGemini } from "../../drizzle/query";
 
 // ==============================
 
-const genAI = new GoogleGenerativeAI(rGemini.apiKey || "");
+export const genAI = new GoogleGenerativeAI(rGemini.apiKey || "");
+export const fileManager = new GoogleAIFileManager(rGemini.apiKey || "");
 
 export const geminiModel = genAI.getGenerativeModel({
   model: rGemini.model,
