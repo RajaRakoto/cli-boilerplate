f9f4a01cb12f818f72ecee6a7f8b6689a7b765df
raja <raja.rakoto7@gmail.com>
2024-11-25 21:15:29 +0300

    test(constants): update test files list

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,15 +4,40 @@ export const DEV_MODE = true;
 // path
 export const FONT_PATH = "./fonts/Standard.flf";
 export const DATABASE_PATH = DEV_MODE ? "drizzle/lucy.db" : "dist/lucy.db";
-export const INPUT_FILES_PATH = DEV_MODE ? "./tests/pdf/" : "./";
+export const ROOT_PATH = "./";
+export const INPUT_FILES_PATH = DEV_MODE ? "./tests/pdf/" : ROOT_PATH;
 
 // mimetype
 export const PDF_MIMETYPE = "application/pdf";
 
 // test
 export const TEST_PDF_FILES = [
+  // "2021-_cv_fadhel_sassi.pdf",
+  // "2023_leveque_livia.pdf",
+  // "2024_03_cv_yeh_zheng_sheng.pdf",
+  // "20240606_cv_ra_gis_legendre.pdf",
+  // "2024_07_-_philippe_popieul_-_cv.pdf",
+  // "2024_kathleen_bonnet.pdf",
+  // "2024-yanis_aitlamine-guenette_cv.pdf",
+  // "2311_marcorelles_benjamin_fr_directeur_des_opa_rations_coo.pdf",
+  // "318_-_cv_philippe_assouline_architecte_infra.pdf",
+  // "947_070309_marc_menage_dircomcvbd_2006.pdf.mescv735495.pdf",
+  "ababacar_kane_cv.pdf",
+  "abdelkrim_assimi.pdf",
+  "abirabdellicv.pdf",
+  "agostino_cembalo_cv_fr.pdf",
+  "arrar_asma_fr.pdf",
+  "aymen_garma.pdf",
+  "aymen_nahdi_project_manager_istqb.pdf",
+  "ayoub_aissa_cv.pdf",
   "bankole.ronel-cv.pdf",
   "ben.smida.cv.qa.pdf",
-  "c.v_martial_samou.pdf",
-  "cv_alexandreagostini_2024_v2.pdf",
+  // "cv_alexandreagostini_2024_v2.pdf",
+  // "cv_anonyme_-_apec.docx.243235440.pdf",
+  // "cv-c-cazeaux-2017.docx.6886551.pdf",
+  // "cv_didier_badart.pdf",
+  // "cv_huynh_van_luc-fr.doc.259314809.pdf",
+  // "c.v_martial_samou.pdf",
+  // "hana-consultante_devops_python.doc.203216361.pdf",
+  // "stephanie_chevance_cv_2024_fr.pdf",
 ];
