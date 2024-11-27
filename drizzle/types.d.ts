c0a775254b4ef3c54ad326ed5ace8330acbd9381
raja <raja.rakoto7@gmail.com>
2024-11-27 17:42:47 +0300

    refactor(types): rename table type and schema type

--- drizzle/types.d.ts
+++ drizzle/types.d.ts
@@ -6,30 +6,30 @@ export type T_tGemini = typeof tGemini;
 export type T_tGeminiUsage = typeof tGeminiUsage;
 export type T_tGeminiSchema = typeof tGemini.$inferInsert;
 export type T_tGeminiUsageSchema = typeof tGeminiUsage.$inferInsert;
-export type T_qInsertTable = T_tGemini | T_tGeminiUsage;
-export type T_qInsertValues = T_tGeminiSchema | T_tGeminiUsageSchema;
+export type T_AllGeminiTables = T_tGemini | T_tGeminiUsage;
+export type T_AllGeminiSchemas = T_tGeminiSchema | T_tGeminiUsageSchema;
 
-export interface I_Gemini {
-	geminiId: number;
-	apiKey: string | null;
-	model: string;
-	prompt: string;
+export interface I_tGemini {
+  geminiId: number;
+  apiKey: string | null;
+  model: string;
+  prompt: string;
 }
 
-export interface I_GeminiUsage {
-	geminiUsageId: number;
-	rpmLimit: number;
-	rpdLimit: number;
-	tpmLimit: number;
-	rpmStart: string | null;
-	rpdStart: string | null;
-	tpmStart: string | null;
-	rpmEnd: string | null;
-	rpdEnd: string | null;
-	tpmEnd: string | null;
-	rpmCounter: number | null;
-	rpdCounter: number | null;
-	tpmCounter: number | null;
-	intervalDelay: number | null;
-	breakDelay: number | null;
+export interface I_tGeminiUsage {
+  geminiUsageId: number;
+  rpmLimit: number;
+  rpdLimit: number;
+  tpmLimit: number;
+  rpmStart: number | null;
+  rpdStart: number | null;
+  tpmStart: number | null;
+  rpmEnd: number | null;
+  rpdEnd: number | null;
+  tpmEnd: number | null;
+  rpmCounter: number | null;
+  rpdCounter: number | null;
+  tpmCounter: number | null;
+  intervalDelay: number | null;
+  breakDelay: number | null;
 }
